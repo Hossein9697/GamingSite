0e339d25698c2d6499ca1352c9cf9c2c25c49d8f
Hossein9697 <hheyatzadeh@gmail.com>
2022-03-22 14:25:15 +0430

    adding progress bar for show game.

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -63,8 +63,8 @@ class GamesController extends Controller
             'genres' => collect($game['genres'])->pluck('name')->implode(', '),
             'involvedCompanies' => isset($game['involved_companies']) ? collect($game['involved_companies'])->pluck('company')->pluck('name')->implode(', ') : 'Unknown Companies',
             'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
-            'memberRating' => isset($game['rating']) ? round($game['rating']) . '%' : '0%',
-            'criticRating' => isset($game['aggregated_rating']) ? round($game['aggregated_rating']) . '%' : '0%',
+            'memberRating' => isset($game['rating']) ? round($game['rating']) : 0,
+            'criticRating' => isset($game['aggregated_rating']) ? round($game['aggregated_rating']) : 0,
             'trailer' => isset($game['videos']) ? 'https://youtube.com/watch/' . $game['videos'][0]['video_id'] : '',
             'screenshots' => isset($game['screenshots']) ? collect($game['screenshots'])->map(function ($screenshot) {
                 return [
@@ -75,7 +75,7 @@ class GamesController extends Controller
             'similar_games' => collect($game['similar_games'])->map(function ($similarGame) {
                 return collect($similarGame)->merge([
                     'coverImageUrl' => isset($similarGame['cover']) ? Str::replaceFirst('thumb', 'cover_big', $similarGame['cover']['url']) : '',
-                    'rating' => isset($similarGame['rating']) ? round($similarGame['rating']) . '%' : null,
+                    'rating' => isset($similarGame['rating']) ? round($similarGame['rating']) : 0,
                     'platforms' => isset($similarGame['platforms']) ? collect($similarGame['platforms'])->pluck('abbreviation')->implode(', ') : 'Unknown Platform'
                 ]);
             })->take(6),
